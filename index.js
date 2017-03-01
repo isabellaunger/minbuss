58f9b421ed20ef535667d8d0b91f80fa8d1bbdfc
isabellaunger <isabella.unger@hotmail.com>
2017-03-01 14:38:47 +0100

    Updated port config for express

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.send('Min Buss!<br>test2')
-})
+  res.send('Min Buss!<br>test2');
+});
 
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
